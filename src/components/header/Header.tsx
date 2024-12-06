6e65b7ea8a3d04c514120939c9bad5e9c45e2df3
cruzito001 <doctor.printer-0k@icloud.com>
2024-12-06 03:47:17 -0600

    Add header, footer

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import styles from "./Header.module.css";
+
+const Header: React.FC = () => {
+  return (
+    <header className={styles.header}>
+      <div className={styles.container}>
+        <a href="/" className={styles.logo}>
+          JGM<span>FUT</span>
+        </a>
+        <nav className={styles.nav}>
+          <a href="/downloads">Downloads</a>
+          <a href="/faq">FAQ</a>
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
